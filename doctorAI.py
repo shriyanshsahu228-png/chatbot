1952f68b50f7827c35ac6c949ae42f20c72d80c1
codewith_shriyansh <shriyanshsahu228@gmail.com>
2026-03-01 00:18:10 +0530

    Add files via upload

---
+++ doctorAI.py
@@ -0,0 +1,93 @@
+#streamlit run doctorAI.py
+import streamlit as st
+from google import genai
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+client = genai.Client()
+st.markdown(
+    """
+    <h1 style="
+         text-align: center;
+        font-size: 42px;
+        background: linear-gradient(to right, #15ea89, #30b478, #7de4b5);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-weight: bold;
+        margin-bottom: 30px;
+    ">
+     WELCOME TO YOUR PERSONAL AI DOCTOR ASSISTANT
+    </h1>
+""",
+    unsafe_allow_html=True
+)
+
+# if "history" not in st.session_state:
+#     st.session_state.history = []
+#text = st.text_input("Describe your Symptoms:")
+
+#if text:
+        
+        # lang= st.selectbox(
+        #     "Select language to be Translate",["English","Hindi","Marathi","Punjabi","Telugu","French","Spanish"]
+        # )
+#if st.button("Consult AI Doctor") and text:
+
+if "history" not in st.session_state:
+        st.session_state.history = []
+
+with st.form("doctor_form", clear_on_submit=True):
+        text = st.text_input("Describe your symptoms:")
+        submitted = st.form_submit_button("Consult")
+
+if submitted and text: 
+        st.chat_message("user").write(text)
+        st.session_state.history.append(f"user:{text}")                   
+# with st.chat_message("user"):
+#     st.write(text)
+
+#st.session_state.history.append(f"user :{text}")    
+        with st.spinner("Analyzing your symptoms,Please Hold few seconds..."):
+
+                prompt = f"""
+        You are a multilingual professional  AI medical doctor assistant.
+
+        conversation history:
+        {st.session_state.history}
+
+        current user message:
+        {text}
+        Rules:
+        - Detect user's language and reply in same language.
+        - Be calm, polite and reassuring like a real doctor.
+        - Ask follow-up questions if needed.
+        - Give simple explanations.
+        - Suggest precautions.
+        - If symptoms serious → mark EMERGENCY.
+        - Suggest which specialist doctor to consult.
+        - Never give dangerous advice.
+
+        Format response strictly like:
+            1. Understanding of problem
+            2. Possible cause
+            3. Precautions
+            4. When to see doctor
+            """
+
+                response = client.models.generate_content(
+                    model="gemini-2.5-flash",
+                    contents=prompt
+                        )
+                reply = response.text if response.text else "No responce generated"
+
+                st.session_state.history.append(f"Doctor:{reply}")
+                st.chat_message("assistant").write(reply)
+                                    # st.success("completed")
+                                    # st.write(reply)
+
+                if "EMERGENCY" in reply.upper():
+                    st.error("⚠️ Emergency symptoms detected.Seek medical help immediately.")
+                else:
+                    st.write("Enter symptoms and click button to consult AI doctor.")    
+
